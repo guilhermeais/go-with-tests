84c108300f249c6840ff316bbe3435ce5f4ab8a5
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-08-21 07:37:48 -0300

    test: use subtests

--- hello_test.go
+++ hello_test.go
@@ -3,19 +3,30 @@ package main
 import "testing"
 
 func TestHello_World(t *testing.T) {
-	got := Hello()
-	want := "Hello, World!"
+	t.Run("saying hello to world when a empty string is supplied", func(t *testing.T) {
+		got := Hello()
+		want := "Hello, World!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 
-func TestHello_Guilherme(t *testing.T) {
-	got := Hello("Guilherme")
-	want := "Hello, Guilherme!"
+	t.Run("saying hello to Guilherme", func(t *testing.T) {
+		got := Hello("Guilherme")
+		want := "Hello, Guilherme!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("saying hello to Guilherme Teixeira", func(t *testing.T) {
+		got := Hello("Guilherme", "Teixeira")
+		want := "Hello, Guilherme Teixeira!"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
