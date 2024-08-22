9574a98f8eeebd456d90c08c856c347744a940e3
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-08-22 10:57:26 -0300

    feat: implements sum all

---
+++ sum/sum_all_test.go
@@ -0,0 +1,21 @@
+package sum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func BenchmarkSumAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAll([]int{1, 2}, []int{0, 9})
+	}
+}
