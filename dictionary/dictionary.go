eb525b8e87855c363aff131dc390c1625851b985
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-09-05 09:09:03 -0300

    feat: add erro handling on add method

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -12,6 +12,14 @@ func (err ErrWordNotFound) Error() string {
 	return fmt.Sprintf("could not find the word %q", err.word)
 }
 
+type ErrWordExists struct {
+	word string
+}
+
+func (err ErrWordExists) Error() string {
+	return fmt.Sprintf("word %q already has a definition", err.word)
+}
+
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
@@ -23,6 +31,12 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
+func (d Dictionary) Add(word, definition string) error {
+	_, wordAlreadyExists := d[word]
+	if wordAlreadyExists {
+		return ErrWordExists{word}
+	}
+
 	d[word] = definition
+	return nil
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -23,12 +23,26 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
-	dictionary.Add(word, definition)
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+		dictionary.Add(word, definition)
 
-	assertDefinition(t, dictionary, word, definition)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{
+			word: definition,
+		}
+		err := dictionary.Add(word, "other definition")
+
+		assertError(t, err, ErrWordExists{word})
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
