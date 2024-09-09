232fc8b5893f08a82f3d6e2a526bca5f9a263e76
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-09-09 14:21:40 -0300

    feat: create counter

---
+++ counter/countdown.go
@@ -0,0 +1,18 @@
+package counter
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Fprint(out, finalWord)
+}
--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"go-with-tests/counter"
 	"go-with-tests/dependencyinjection"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
+func dependencyInjectionExample() {
 	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(dependencyinjection.MyGreetHandler)))
+}
+
+func countdownExample() {
+	counter.Countdown(os.Stdout)
+}
+func main() {
+	go dependencyInjectionExample()
+	countdownExample()
 }
