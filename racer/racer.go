ff2fe7e732202dc122ca00273979165197a9f800
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-09-12 12:58:36 -0300

    feat: create racer
    racer compare two urls latency and see what is fastest

---
+++ racer/racer.go
@@ -0,0 +1,25 @@
+package racer
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(urls ...string) (winner string) {
+	var fastest time.Duration = 0
+	for i, url := range urls {
+		start := time.Now()
+		http.Get(url)
+		duration := time.Since(start)
+
+		isFirstUrl := i == 0
+
+		if isFirstUrl || duration < fastest {
+			fastest = duration
+			winner = url
+			continue
+		}
+	}
+
+	return winner
+}
