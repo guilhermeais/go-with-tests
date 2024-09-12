577be4f75d5414303a4ba463c6fd2a77621755f0
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-09-12 13:14:13 -0300

    test: add benchmark on racer

--- racer/racer_test.go
+++ racer/racer_test.go
@@ -3,16 +3,16 @@ package racer
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 	"time"
 )
 
 func TestRacer(t *testing.T) {
-
-	slowServer := makeDelayedServer(0)
+	slowServer := makeDelayedServer(20 * time.Millisecond)
 	defer slowServer.Close()
 
-	fastServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0)
 	defer fastServer.Close()
 
 	slowUrl := slowServer.URL
@@ -24,7 +24,31 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("Got %q, want %q", got, want)
 	}
+}
 
+func BenchmarkRacer(b *testing.B) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		delayStr := query.Get("delay")
+		delay, err := strconv.Atoi(delayStr)
+		if err != nil {
+			delay = 0
+		}
+
+		time.Sleep(time.Duration(delay) * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	urls := make([]string, 50)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = server.URL + "?delay=" + strconv.Itoa(i+1)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Racer(urls...)
+	}
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
