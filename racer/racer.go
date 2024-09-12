6cc15c37b23431baac15c104ac8f3dfb0210ff2d
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-09-12 13:24:59 -0300

    perf: use goroutines to improve perf

--- racer/racer.go
+++ racer/racer.go
@@ -2,24 +2,24 @@ package racer
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(urls ...string) (winner string) {
-	var fastest time.Duration
-	for i, url := range urls {
-		start := time.Now()
-		http.Get(url)
-		duration := time.Since(start)
-
-		isFirstUrl := i == 0
-
-		if isFirstUrl || duration < fastest {
-			fastest = duration
-			winner = url
-			continue
-		}
+	ch := make(chan string)
+	for _, url := range urls {
+		go func(_url string) {
+			ping(url)
+			ch <- _url
+		}(url)
 	}
 
-	return winner
+	return <-ch
+}
+
+func ping(url string) {
+	resp, err := http.Head(url)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
 }
