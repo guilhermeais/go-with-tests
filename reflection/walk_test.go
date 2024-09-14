d194aaa41d34ccb48494dd9204fdaacf23ee2471
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-09-14 11:25:54 -0300

    feat(walk): create a function to walk through interfaces fields

---
+++ reflection/walk_test.go
@@ -0,0 +1,117 @@
+package reflection
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestWal(t *testing.T) {
+	type Profile struct {
+		Age  int
+		City string
+	}
+
+	type Person struct {
+		Name    string
+		Profile Profile
+	}
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			Name: "struct with one string field",
+			Input: struct {
+				Name string
+			}{"Chris"},
+			ExpectedCalls: []string{"Chris"},
+		},
+		{
+			Name: "struct with two string fields",
+			Input: struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			ExpectedCalls: []string{"Chris", "London"},
+		},
+		{
+			Name: "struct with non string field",
+			Input: struct {
+				Name string
+				Age  int
+			}{"Chris", 20},
+			ExpectedCalls: []string{"Chris"},
+		},
+		{
+			Name: "struct with nested fields",
+			Input: Person{
+				Name: "Chris",
+				Profile: Profile{
+					Age:  20,
+					City: "London",
+				},
+			},
+			ExpectedCalls: []string{"Chris", "London"},
+		},
+		{
+			Name: "struct with fields pointing to things",
+			Input: &Person{
+				Name: "Chris",
+				Profile: Profile{
+					Age:  20,
+					City: "London",
+				},
+			},
+			ExpectedCalls: []string{"Chris", "London"},
+		},
+		{
+			Name: "struct with slices",
+			Input: []Profile{
+				{Age: 20,
+					City: "London"},
+				{Age: 20,
+					City: "São Paulo"},
+			},
+			ExpectedCalls: []string{"London", "São Paulo"},
+		},
+		{
+			Name: "struct with arrays",
+			Input: [2]Profile{
+				{Age: 20,
+					City: "London"},
+				{Age: 20,
+					City: "São Paulo"},
+			},
+			ExpectedCalls: []string{"London", "São Paulo"},
+		},
+		{
+			Name: "struct with maps",
+			Input: map[string]string{
+				"Cow":   "Moo",
+				"Sheep": "Baa",
+			},
+			ExpectedCalls: []string{"Moo", "Baa"},
+		},
+	}
+
+	for i, test := range cases {
+		t.Run(fmt.Sprintf("[%d] %q", i, test.Name), func(t *testing.T) {
+
+			var got []string = []string{}
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if len(got) != len(test.ExpectedCalls) {
+				t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+			}
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
