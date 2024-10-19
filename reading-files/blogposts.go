79f7f60100a0396ca45c326d7d50f78aa79c2661
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-10-19 11:20:26 -0300

    refactor: improve abstractions of reading a post from file

--- reading-files/blogposts.go
+++ reading-files/blogposts.go
@@ -15,7 +15,7 @@ func PostsFromFS(fileSystem fs.FS) []Post {
 	posts := []Post{}
 	if err == nil {
 		for _, file := range dir {
-			posts = append(posts, makePostFromFile(fileSystem, file))
+			posts = append(posts, makePostFromFile(fileSystem, file.Name()))
 		}
 
 		return posts
@@ -23,9 +23,13 @@ func PostsFromFS(fileSystem fs.FS) []Post {
 	return nil
 }
 
-func makePostFromFile(fileSystem fs.FS, file fs.DirEntry) Post {
-	blogFile, _ := fileSystem.Open(file.Name())
-	fileContents, _ := io.ReadAll(blogFile)
+func makePostFromFile(fileSystem fs.FS, filename string) Post {
+	blogFile, _ := fileSystem.Open(filename)
+	return newPost(blogFile)
+}
+
+func newPost(blogReader io.Reader) Post {
+	fileContents, _ := io.ReadAll(blogReader)
 
 	title := strings.TrimPrefix(string(fileContents), "Title: ")
 	return Post{Title: title}
