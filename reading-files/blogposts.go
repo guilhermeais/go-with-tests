4466e47b093f16a923bec5bae4df3c50ed82c7fe
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-10-19 11:17:27 -0300

    feat: extracting title from blog file

--- reading-files/blogposts.go
+++ reading-files/blogposts.go
@@ -1,20 +1,32 @@
 package blogposts
 
 import (
+	"io"
 	"io/fs"
+	"strings"
 )
 
-type Post struct{}
+type Post struct {
+	Title string
+}
 
-func PostsFromFS(fileSysten fs.FS) []Post {
-	dir, err := fs.ReadDir(fileSysten, ".")
+func PostsFromFS(fileSystem fs.FS) []Post {
+	dir, err := fs.ReadDir(fileSystem, ".")
 	posts := []Post{}
 	if err == nil {
-		for range dir {
-			posts = append(posts, Post{})
+		for _, file := range dir {
+			posts = append(posts, makePostFromFile(fileSystem, file))
 		}
 
 		return posts
 	}
 	return nil
+}
+
+func makePostFromFile(fileSystem fs.FS, file fs.DirEntry) Post {
+	blogFile, _ := fileSystem.Open(file.Name())
+	fileContents, _ := io.ReadAll(blogFile)
+
+	title := strings.TrimPrefix(string(fileContents), "Title: ")
+	return Post{Title: title}
 }
--- reading-files/blogposts_test.go
+++ reading-files/blogposts_test.go
@@ -8,13 +8,18 @@ import (
 
 func TestBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello-world.md":  {Data: []byte("hello, world")},
-		"hello-twitch.md": {Data: []byte("hello, twitch")},
+		"1-hello-world.md": {Data: []byte("Title: Hello, TDD world!")},
+		"hello-twitch.md":  {Data: []byte("Title: Hello, twitchy world!")},
 	}
 
 	posts := blogposts.PostsFromFS(fs)
 
 	if len(posts) != len(fs) {
 		t.Errorf("expected %d posts, got %d posts", len(fs), len(posts))
+	}
+
+	expectedFirstPost := blogposts.Post{Title: "Hello, TDD world!"}
+	if posts[0] != expectedFirstPost {
+		t.Errorf("got %#v, want %#v", posts[0], expectedFirstPost)
 	}
 }
